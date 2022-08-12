148fb19082c75c72115c46c79978fc395fa221cd
tewelle <tewelle.welemariam@cag.se>
2022-08-12 08:25:36 -0500

    Refactored

--- exercise/js/hoisting.js
+++
@@ -1,8 +0,0 @@
-
-
-console.log("a: " + a); //undefined
-b(); // function is called!
-var a = 5; // if we remove this line we get error 'a is not defined'
-function b() {
-    console.log("function is called!");
-}
